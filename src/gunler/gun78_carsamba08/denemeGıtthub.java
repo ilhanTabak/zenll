44b26770c934760a002f7017c2a612f0fbd8f43d
ilhanTabak <herr.ilhantabak@gmail.com>
2023-02-17 16:09:20 +0100

    hello workd

---
+++ src/gunler/gun78_carsamba08/denemeGıtthub.java
@@ -0,0 +1,10 @@
+package gunler.gun78_carsamba08;
+
+import javax.crypto.spec.PSource;
+
+public class denemeGıtthub {
+
+    public static void main(String[] args) {
+        System.out.printf("hello world");
+    }
+}
