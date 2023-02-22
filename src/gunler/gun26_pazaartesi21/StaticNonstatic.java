6caedcd16cd7342d60f048838b5c4ddaf6381bb6
ilhanTabak <herr.ilhantabak@gmail.com>
2023-02-22 19:20:27 +0100

    Static nonstatic

---
+++ src/gunler/gun26_pazaartesi21/StaticNonstatic.java
@@ -0,0 +1,35 @@
+package gunler.gun26_pazaartesi21;
+
+public class StaticNonstatic {
+
+    public void main(String[] args) {
+
+
+
+        Kisi kisi=new Kisi();
+
+        kisi.isim="Ali";
+
+        Kisi kisi2=new Kisi();
+        kisi2.isim="Betül";
+
+
+
+
+
+    }
+
+
+
+    class Kisi{
+
+        int yas;
+        String isim;
+
+
+
+
+    }
+
+
+}
---
+++ src/gunler/gun26_pazaartesi21/YeniFonksiyonCalismasi.java
@@ -0,0 +1,35 @@
+package gunler.gun26_pazaartesi21;
+
+public class YeniFonksiyonCalismasi {
+
+
+
+
+    public static void ucunKatlari(){
+
+        for (int i = 1; i <=100 ; i++) {
+
+            if (i%3==0) {
+
+                System.out.print(" "+i+" ");
+
+            }
+
+        }
+
+
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+        ucunKatlari();
+
+
+
+    }
+}
