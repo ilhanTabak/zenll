c0854705c1929efdc61516825b73b3646529f922
ilhanTabak <herr.ilhantabak@gmail.com>
2023-03-06 00:29:20 +0100

    fakan

---
+++ src/gunler/gun4_persembe20/azaltma.java
@@ -0,0 +1,16 @@
+package gunler.gun4_persembe20;
+
+public class azaltma {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Sonuc" + (5 == 5));
+        System.out.println("Sonuc" + (8 != 8));
+        System.out.println("Sonuc" + (8 > 5));
+
+
+    }
+}
+
+
